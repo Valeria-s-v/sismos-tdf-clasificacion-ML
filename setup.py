444176cf3c07fd14e465dd0d506a2741df7656c3
Valeria-s-v <valeria.s.villegas@gmail.com>
2025-05-12 21:56:20 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Clasificación de eventos sísmicos',
+    author='Valeria Villegas',
+    license='MIT',
+)
